171e94d07322dc194ef65e7a1e5a9d2fed4e938c
KeZ <zhangke0607@gmail.com>
2022-01-18 21:24:55 +1030

    task3 failed
    cannot use macro query to CRUD.
    tried multiple ways, but cannot fix it.

--- examples/databases/src/sqlx.rs
+++ examples/databases/src/sqlx.rs
@@ -22,13 +22,13 @@ struct Post {// struct edited, viewedTimes added
     text: String,
 
     //#[serde(skip_deserializing)]
-    //viewedTimes: i64,
+    viewedTimes: i64,
 }
 
 #[post("/post", data = "<post>")]//use post to raise a new post
 async fn create(mut db: Connection<Db>, post: Json<Post>) -> Result<Created<Json<Post>>> {
     // There is no support for `RETURNING`.
-    sqlx::query!("INSERT INTO posts (title, text) VALUES (?, ?)", post.title, post.text)
+    sqlx::query!("INSERT INTO posts (title, text, viewedTimes) VALUES (?, ?, ?)", post.title, post.text, post.viewedTimes)
         .execute(&mut *db)
         .await?;
 
@@ -48,9 +48,9 @@ async fn list(mut db: Connection<Db>) -> Result<Json<Vec<i64>>> {
 
 #[get("/get/<id>")]// use get with id to show the data with entered id
 async fn read(mut db: Connection<Db>, id: i64) -> Option<Json<Post>> {
-    sqlx::query!("SELECT id, title, text FROM posts WHERE id = ?", id)//viewed times added
+    sqlx::query!("SELECT id, title, text, viewedTimes FROM posts WHERE id = ?", id)//viewed times added
         .fetch_one(&mut *db)
-        .map_ok(|r| Json(Post { id: Some(r.id), title: r.title, text: r.text }))
+        .map_ok(|r| Json(Post { id: Some(r.id), title: r.title, text: r.text, viewedTimes: r.viewedTimes }))
         .await
         .ok()
 }
